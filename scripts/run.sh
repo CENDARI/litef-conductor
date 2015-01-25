ee646a6788036f00a86bd6adfa52a93ddc2eef1f
Ivan Čukić <ivan.cukic@kde.org>
2015-01-26 00:30:50 +0100

    Updated the run.sh to include deps/*jar in the classpath

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,5 +2,17 @@
 echo "Starting..."
 cd /opt/litef
 # java7/bin/java -Dconfig.resource=/opt/litef/etc/application.conf -jar litef-conductor.jar &> /dev/null &
-java7/bin/java -cp etc:litef-conductor.jar core.Rest  &> /dev/null &
+
+# java7/bin/java -cp etc:litef-conductor.jar core.Rest  &> /dev/null &
+
+CPATH="etc:litef-conductor.jar"
+
+for dep in deps/*jar;
+do
+    CPATH=$CPATH:$dep
+done
+
+echo $CPATH
+
+java7/bin/java -cp $CPATH core.Rest  &> /var/log/litef.log-`date +%Y%m%d` &
 
